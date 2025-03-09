d9c44a426410089b91d7123c90cd95ad47986ddf
ca0ca0 <saonv.bkrd@gmail.com>
2025-03-09 22:28:15 +0700

    add lib to ignore run tsc

---
+++ lib/index.d.ts
@@ -0,0 +1,9 @@
+export declare const setBrightnessLevel: (brightnessLevel: number) => Promise<void>;
+export declare const getBrightnessLevel: () => Promise<number>;
+export declare const getSystemBrightnessLevel: () => Promise<number>;
+declare const _default: {
+    setBrightnessLevel: (brightnessLevel: number) => Promise<void>;
+    getBrightnessLevel: () => Promise<number>;
+    getSystemBrightnessLevel: () => Promise<number>;
+};
+export default _default;
